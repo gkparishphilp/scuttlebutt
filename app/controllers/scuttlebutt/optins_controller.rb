85b27cbeffa0c7a7ae05086213a32f05ed8875bf
Michael Ferguson <meister@spacekace.com>
2018-09-11 12:50:16 -0700

    fix optins controller

--- app/controllers/scuttlebutt/optins_controller.rb
+++ app/controllers/scuttlebutt/optins_controller.rb
@@ -1,35 +1,57 @@
 module Scuttlebutt
-	
+
 	class OptinsController < ApplicationController
-		@user = User.find_or_create_by_email( params[:message][:email] )
-		@user.full_name = params[:message][:name]
 
-		if @user.save
-			log_event( { name: 'email_optin', content: "opted into the email list" } )
-			respond_to do |format|
-				format.js {
-					render :create
-				}
-				format.json {
-					render :create
-				}
-				format.html {
-					if params[:action] == 'back'
-						set_flash @message
-						redirect_back( fallback_location: thank_you_optins_path
-					else
-						redirect_to thank_you_optins_path
-					end
-				}
+		def create
+			@user = User.find_or_create_by( email: params[:message][:email].downcase )
+			@user.attributes = optin_params
+
+			if @user.save
+
+				@message = 'Success'
+
+				log_event( { user: @user, name: 'email_optin', content: "opted into the email list" } )
+
+				respond_to do |format|
+					format.js {
+						render :create
+					}
+					format.json {
+						render :create
+					}
+					format.html {
+						if params[:action] == 'back'
+							set_flash( @message ) if @message
+							redirect_back( fallback_location: thank_you_optins_path )
+						else
+							redirect_to thank_you_optins_path
+						end
+					}
+				end
+			else
+				@message = "Couldn't sign up #{@user.email}"
+				@success = false
+
+				respond_to do |format|
+					format.js {
+						render :create
+					}
+					format.json {
+						render :create
+					}
+					format.html {
+						set_flash @message, :error
+						redirect_back( fallback_location: root_path )
+					}
+				end
+
 			end
-		else
 
 		end
 
+		private
+			def optin_params
+				params.require( :message ).permit( :full_name, :first_name, :last_name )
+			end
 	end
-
-	private
-		def optin_params
-			params.require( :message ).permit( :email, :name )
-		end
 end
